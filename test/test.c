d6176cd72fc3a842fde14287d5b149ff375a54a0
shiopon01 <shio.0323@gmail.com>
2025-11-30 17:00:12 +0900

    Completion of step0

--- test/test.c
+++ test/test.c
@@ -54,6 +54,11 @@ cleanup(void)
 static int
 app_main(void)
 {
+    debugf("press Ctrl_C to terminate");
+    while (!terminate) {
+        sleep(1);
+    }
+    debugf("terminate");
     return 0;
 }
 
